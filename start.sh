97fb58f7d2f5c7f3e1fa6d4d1aa9dbd74bdea9ce
FabianDav <67824368+FabianDav@users.noreply.github.com>
2022-12-17 13:05:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/FabianDav/Cargador_UniqxBot /CargadorUniqxBot
+git clone https://github.com/FabianDav/Cargador_UniqxBot /Cargador_UniqxBot
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
